2f435c2f3d8faf521165004b589992ab58603841
mhdrare <mhdfaisalakbar206@gmail.com>
2020-12-05 04:09:42 +0700

    Initial commit

---
+++ app/Http/Controllers/VisitController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Visit;
+use Illuminate\Http\Request;
+
+class VisitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $visits = Visit::find(1);
+
+        return view('welcome', ['visits'=>$visits]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Visit  $visit
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Visit  $visit
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Visit $visit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Visit  $visit
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $visits = Visit::find(1);
+
+        $visitCount = $request->visitCount;
+        $visits->visit_count = $visitCount;
+
+        $visits->save();
+
+        return redirect()->route('/');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Visit  $visit
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Visit $visit)
+    {
+        //
+    }
+}
